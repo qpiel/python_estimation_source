a1af75507e25ed214a2da227bbde71fb607abcb5
qpiel <piel@lapp.in2p3.fr>
2017-01-12 13:15:43 +0100

    vreq

--- Script/Script.py
+++ Script/Script.py
@@ -5,37 +5,42 @@ import AlltheSourceFitLPmodel
 import AllSourcesSimulate_Ctools
 import AllSourcesFit_Ctools
 import matplotlib.pyplot as plt
-x = []
-ts = []
+liste =[]
+x = [1,3,10,30,100]
+liste.append(x)
+
+i=0
 with open("Source2.txt") as f:
    for line in f:
+        ts = []
         GetSourceInformation(line.split("\t")[0],"")
         print (line.split("\t")[0])
 
         DoSimulation(line.split("\t")[0],line.split("\t")[5],1)
         tmp=DoFit(line.split("\t")[0],line.split("\t")[5],1)
         ts.append(tmp)
-        x.append(1)
+        
 
         DoSimulation(line.split("\t")[0],line.split("\t")[5],3)
         tmp=DoFit(line.split("\t")[0],line.split("\t")[5],3)
         ts.append(tmp)
-        x.append(3)
+        
 
         DoSimulation(line.split("\t")[0],line.split("\t")[5],10)
         tmp=DoFit(line.split("\t")[0],line.split("\t")[5],10)
         ts.append(tmp)
-        x.append(10)
+        
 
         DoSimulation(line.split("\t")[0],line.split("\t")[5],30)
         tmp=DoFit(line.split("\t")[0],line.split("\t")[5],30)
         ts.append(tmp)
-        x.append(30)
+        
 
         DoSimulation(line.split("\t")[0],line.split("\t")[5],100)
         tmp=DoFit(line.split("\t")[0],line.split("\t")[5],100)
         ts.append(tmp)
-        x.append(100)
+        
+        liste.append(ts)
 
         plt.plot(x,ts,label = line.split("\t")[0])
         plt.legend(loc=3)
@@ -43,4 +48,5 @@ with open("Source2.txt") as f:
         plt.xlabel('Simulation time (h)')
         plt.title(str(source))
         plt.show()
+        i = i+1
 
